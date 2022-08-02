faa2a425526f46c9746f90584f1aa6bade77c7c4
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-02 18:20:18 +0300

    Change default route

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:drafting_dan/src/home/home_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -73,7 +74,7 @@ class MyApp extends StatelessWidget {
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const HomeRoute();
                 }
               },
             );
